35da1c4ab88f20bce202a465c98ff7884272cf39
Bano Qabil <teacher@banoqabil.com>
2023-04-04 15:10:06 +0500

    FEAT: JS Intro Class

---
+++ scripts/app.js
@@ -0,0 +1,45 @@
+// alert("Hello JS")
+// var user = prompt("Your Name PLease", "Usama")
+// alert(user)
+
+// var arr = ['hello', 'hi']
+// document.write(arr)
+// console.log(arr)
+
+var yusraImranBanoQabil = "jee Sahi kaha , yeh pagal hai"; //camelCase
+var YusraImranBanoQabil; // PascalCase
+var yusra_imran_bano_qabil //snake_case
+
+// console.log(yusraImranBanoQabil)
+// var num = 13
+// console.log(num)
+
+// var num1 =+ prompt("Num 1 Please")
+// var num2 =+ prompt("Num 2 Please")
+
+// console.log(Number(num1) + Number(num2))
+// console.log(num1+num2)
+
+// console.log("1"+"2")
+
+// console.log(2+6)
+// console.log(2-6)
+// console.log(2*6)
+// console.log(2/6)
+// console.log(2**6)
+
+// console.log(((1+(6/7))*(8/9)+6)-(12*5)/3)
+// var name = "Usama"
+// var fname = "Usman"
+// console.log(name+ " " + fname)
+
+var num = 2
+console.log(num)
+// num = num+10
+// num *=10
+// num++
+++num
+console.log(num)
+
+
+
