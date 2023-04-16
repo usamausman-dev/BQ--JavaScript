15a963027ca8639297789b87fc2f4ee0090f2711
Usama Usman <50479258+usamausman-dev@users.noreply.github.com>
2023-04-15 22:32:41 -0700

    FEAT : JS  Chap 10-22

--- scripts/app.js
+++ scripts/app.js
@@ -33,6 +33,7 @@
 // console.log(((1+(6/7))*(8/9)+6)-(12*5)/3)
 // var name = "Usama"
 // var fname = "Usman"
+
 // console.log(name+ " " + fname)
 
 // var num = 2
@@ -43,7 +44,91 @@
 // ++num
 // console.log(num)
 
-// DAY 2 - 5 APR 
+// DAY 2 
+
+// var user = "Abdullah"
+
+// if (user == 'usama') {
+//     console.log("Welcome Sr")
+// }
+
+// else if (user == 'abdullah') {
+//     console.log('Please On the server')
+// }
+
+// else {
+//     console.log('Go to your classroom')
+// }
+
+// var num = 5
+
+// if(num != 5){
+//     console.log('correct')
+// }
+// else{
+//     console.log('wrong')
+// }
+
+// console.log(num >= 5)
+
+// var nationality = prompt('Your Nationality?')
+// var age = 19
+
+// if (nationality == 'pakistani' || nationality == 'Pakistani') {
+//     if (age >= 18) {
+//         console.log('go to the voting room')
+//     }
+//     else {
+//         console.log('you are under age')
+//     }
+
+// }
+// else {
+//     console.log("You're not eligible")
+// }
+
+
+// if ((nationality == 'pakistani' || nationality == 'Pakistani' || nationality=='PAKISTANI') && age>=18) {
+//     console.log('Go to the voting Room')
+// }
+// else {
+//     console.log("You're not eligible")
+// }
+
+// console.log(nationality.toUpperCase())
+
+// if (nationality.toLowerCase() == 'pakistani' && age >= 18) {
+//     console.log('Go to the voting Room')
+// }
+// else {
+//     console.log("You're not eligible")
+// }
+
+var arr = ['zohaib', 'talha', 'shehryar', 'asmat', "haris", "maqsood"]
+// console.log(arr[1])
+// arr[3] ='asmat khan'
+// arr[6] = 'Usama'
+
+// arr.push("Usama","haris")
+// arr.unshift("Usama","haris")
+// var abc = arr.pop()
+// var abc = arr.shift()
+
+// arr.splice(2,1,"haris","maqsood")
+// var abc = arr.slice(1,4)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+
+for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
+
+console.log(arr.length)
+
+
 
 
 
